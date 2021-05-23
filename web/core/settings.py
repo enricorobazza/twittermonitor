dad77d8d21d3d510d3d15ed09d993b52a4118cde
Enrico <enricorobazzi@gmail.com>
2021-05-23 14:56:56 -0300

    Updates static files

--- web/core/settings.py
+++ web/core/settings.py
@@ -157,7 +157,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
--- web/start-server.sh
+++ web/start-server.sh
@@ -3,5 +3,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd twitterkafka; python manage.py createsuperuser --no-input)
 fi
-(cd twitterkafka; gunicorn core.wsgi --user www-data --bind 0.0.0.0:2195 --workers 3) &
+(cd twitterkafka; gunicorn core.wsgi --user www-data --bind 0.0.0.0:2196 --workers 3) &
 nginx -g "daemon off;"
